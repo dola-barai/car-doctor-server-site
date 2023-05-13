aeafdc667ed1690eb90542b6e460a10ea8ad6867
Dola Barai <dola43dc@gmail.com>
2023-05-14 01:38:49 +0600

    update added

--- index.js
+++ index.js
@@ -67,6 +67,25 @@ async function run() {
         res.send(result)
     });
 
+    app.put('/bookings/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const updatedBooking = req.body;
+        console.log(updatedBooking);
+        const updateDoc = {
+            $set: {
+                status: updatedBooking.status
+            },
+        }
+        
+    })
+
+    app.delete('/bookings/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await bookingCollection.deleteOne(query);
+        res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
